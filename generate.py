396babedbbc6ba08c7cb6d8cb294a5cbee8a98f4
Arvin Cudanin <arvin.cudanin@gmail.com>
2019-06-22 12:06:22 +0800

    Added README file, design documentation and modified default values

--- generate.py
+++ generate.py
@@ -12,8 +12,9 @@ parser = argparse.ArgumentParser(description='Python implementation of firewall 
 parser.add_argument('-s', '--source', metavar='PATH', dest='source', type=str, required=True,
                     help='The source csv file to refer the firewall configurations from.')
 
-parser.add_argument('-f', '--filename', metavar='FILENAME', dest='filename', type=str, required=True,
-                     help='The source csv file to refer the firewall configurations from.')
+parser.add_argument('-f', '--filename', metavar='FILENAME', dest='filename', type=str, default='./output.json',
+                     help='The source csv file to refer the firewall configurations from. '
+                          'Default value is \'./output.json\'.')
 
 parser.add_argument('-l', '--logfile', metavar='PATH', dest='logfile', type=str, default='./firewall-json-generator.log',
                     help='Path to logfile. Default value is \'./firewall-json-generator.log\'.')
@@ -26,21 +27,26 @@ class CSVFileHandler(object):
     def readFile(self):
         try:
             csv_file = open(self.path, mode='r', encoding='utf-8-sig')
+            logger.info('Able to read csv file: {}'.format(self.path))
             return csv_file
         except Exception as e:
             logger.error('Error opening csv file. {}'.format(e))
+            exit()
 
     def getDict(self):
         try:
             return csv.DictReader(self.readFile())
         except Exception as e:
             logger.error('Error converting csv to dictionary. {}'.format(e))
+            exit()
 
     def getJSON(self):
         try:
             return json.dumps([row for row in self.getDict()], indent=4, sort_keys=True)
+            logger.info('Able to convert csv to JSON')
         except Exception as e:
             logger.error('Error converting dictionary to json. {}'.format(e))
+            exit()
 
 class FileGenerator(object):
     def __init__(self, json_string, destination_path):
@@ -48,8 +54,14 @@ class FileGenerator(object):
         self.content = json_string
 
     def writeToFile(self):
-        target_file = open(self.path, 'w')
-        target_file.write(self.content)
+        try:
+            target_file = open(self.path, 'w')
+            target_file.write(self.content)
+            logger.info('Able to write to csv file: {}'.format(self.path))
+
+        except Exception as e:
+            logger.error('Error writing to target json path. {}'.format(e))
+            exit()
 
 
 if __name__ == '__main__':
@@ -67,8 +79,10 @@ if __name__ == '__main__':
     logger.addHandler(filehandler)
     logger.setLevel(logging.INFO)
 
-    csvhandler = CSVFileHandler(source_path=args.source)
-    json_output = csvhandler.getJSON()
+    # Read and convert CSV file
+    csv_handler = CSVFileHandler(source_path=args.source)
+    json_output = csv_handler.getJSON()
 
-    filegenerator = FileGenerator(json_string=json_output, destination_path=args.filename)
-    filegenerator.writeToFile()
+    # Write to JSON file
+    file_generator = FileGenerator(json_string=json_output, destination_path=args.filename)
+    file_generator.writeToFile()
